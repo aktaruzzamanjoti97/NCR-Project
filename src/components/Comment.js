65036528e5d9e623fc2d84bae09bd3f31938d84b
M. Aktaruzzaman Joti <63288034+aktaruzzamanjoti97@users.noreply.github.com>
2025-09-15 13:24:44 +0600

    Update Comment.js

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -62,7 +62,7 @@ const Comment = ({ comment, onReply, onUpdate, onDelete, userName, depth }) => {
 							<textarea
 								value={editContent}
 								onChange={(e) => setEditContent(e.target.value)}
-								className='w-full px-3 py-2 border border-gray-200 rounded-lg resize-none focus:outline-none focus:ring-2 focus:ring-blue-500'
+								className='w-full px-3 py-2 border border-gray-200 rounded-lg resize-none focus:outline-none focus:ring-2 focus:ring-blue-500 text-black'
 								rows='3'
 							/>
 							<div className='flex gap-2 mt-2'>
@@ -115,7 +115,7 @@ const Comment = ({ comment, onReply, onUpdate, onDelete, userName, depth }) => {
 								value={replyContent}
 								onChange={(e) => setReplyContent(e.target.value)}
 								placeholder={`Reply to ${comment.author}...`}
-								className='w-full px-3 py-2 border border-gray-200 rounded-lg resize-none focus:outline-none focus:ring-2 focus:ring-blue-500'
+								className='w-full px-3 py-2 border border-gray-200 rounded-lg resize-none focus:outline-none focus:ring-2 focus:ring-blue-500 text-black'
 								rows='2'
 							/>
 							<div className='flex gap-2 mt-2'>
